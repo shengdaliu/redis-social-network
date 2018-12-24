05fe1e8cfc171a31f164a630cf9d823684e8efc8
Shengda Liu <liu.shengda@hotmail.com>
2018-12-24 16:08:08 +0100

    update(api): step 8 comment system on posts

--- routes/api.js
+++ routes/api.js
@@ -9,12 +9,10 @@ var redis = require("redis"),
 router.get('/', (req, res) => {
   res.send('respond with hello');
 });
-
 // Test get user
 router.get('/user', (req, res) => {
   res.send('Pls give an uid');
 });
-
 // Test get user
 router.get('/user/:uid', (req, res) => {
   var uid = "user:" + req.params.uid;
@@ -38,7 +36,7 @@ router.get('/user/:uid', (req, res) => {
     res.send(JSON.stringify(obj));
   });
 });
-
+// Get friend invitation sent list of a user
 router.get('/user/:uid/sentList', (req, res) => {
 
   var uid = req.params.uid;
@@ -53,7 +51,7 @@ router.get('/user/:uid/sentList', (req, res) => {
     res.send(JSON.stringify(reply));
   });
 });
-
+// Get received friend invitation list of a user
 router.get('/user/:uid/receivedList', (req, res) => {
 
   var uid = req.params.uid;
@@ -68,7 +66,7 @@ router.get('/user/:uid/receivedList', (req, res) => {
     res.send(JSON.stringify(reply));
   });
 });
-
+// Get friend list of a user
 router.get('/user/:uid/friendList', (req, res) => {
 
   var uid = req.params.uid;
@@ -83,7 +81,7 @@ router.get('/user/:uid/friendList', (req, res) => {
     res.send(JSON.stringify(reply));
   });
 });
-
+// Get status post list of a user
 router.get('/user/:uid/postList', (req, res) => {
 
   var uid = req.params.uid;
@@ -98,9 +96,29 @@ router.get('/user/:uid/postList', (req, res) => {
     res.send(JSON.stringify(reply));
   });
 });
+// Get comment list of a specific post of a specific user
+router.get('/user/:uid/post/:postId/commentList', (req, res) => {
+
+  var uid = req.params.uid;
+  var postId = req.params.postId;
+
+  // Check Redis is running without error
+  client.on("error", function (err) {
+    console.log("Error " + err);
+  });
+
+  client.keys("user:" + uid + ":post:" + postId + ":comment:*", function (err, comments) {
+    if (err) return console.log(err);
+    console.log(comments);
+    comments.forEach(function (commentId) {
+      console.log("Comment : " + commentId.toString());
+    });
+    res.send(JSON.stringify(comments));
+  });
+});
 
 ////////////////////// POST ////////////////////////////
-
+// Request for adding a user as friend
 router.post('/user/:uid/addFriend', (req, res) => {
 
   var uid = req.params.uid;
@@ -116,7 +134,7 @@ router.post('/user/:uid/addFriend', (req, res) => {
 
   res.send("Request sent");
 });
-
+// Accept a friend request
 router.post('/user/:uid/acceptRequest', (req, res) => {
 
   var uid = req.params.uid;
@@ -134,7 +152,7 @@ router.post('/user/:uid/acceptRequest', (req, res) => {
 
   res.send("Request accepted");
 });
-
+// Delete a friend
 router.post('/user/:uid/deleteFriend', (req, res) => {
 
   var uid = req.params.uid;
@@ -150,13 +168,13 @@ router.post('/user/:uid/deleteFriend', (req, res) => {
 
   res.send("friend deleted");
 });
-
+// Create a status post
 router.post('/user/:uid/addPost', (req, res) => {
 
   var uid = req.params.uid;
   var post = {
     postId: "user:" + uid + ":post:",
-    description: req.query.description
+    message: req.query.message
   };
 
   // Check Redis is running without error
@@ -169,36 +187,89 @@ router.post('/user/:uid/addPost', (req, res) => {
       console.log("Error " + err);
     }
     post.postId += reply
-    // Create the post
-    client.set(post.postId, post.description, function (err, reply) {
-      if (err) {
-        console.log("Error " + err);
-      }
-    })
-    // Add the post to user's post list
-    client.rpush("user:" + uid + ":postList", post.postId, function (err, reply) {
-      if (err) {
-        console.log("Error " + err);
-      }
-    })
-    // Add the post to all friends' updates list
-    client.lrange("user:" + uid + ":friendList", 0, -1, function (err, friends) {
-      // console.log(friends);
-      friends.forEach(function (friend) {
-        client.rpush("user:" + friend + ":updateList", post.postId, function (err, reply) {
-          if (err) {
-            console.log("Error " + err);
-          }
-        })
+
+    client.multi()
+      // Create the post using HSET with a specific message and datetime
+      .hmset(post.postId, ["message", post.message, "datetime", Date.now()], function (err, reply) {
+        if (err) {
+          console.log("Error " + err);
+        }
+      })
+      // Add the post to user's post list
+      .rpush("user:" + uid + ":postList", post.postId, function (err, reply) {
+        if (err) {
+          console.log("Error " + err);
+        }
+      })
+      // Add the post to all friends' updates list
+      .lrange("user:" + uid + ":friendList", 0, -1, function (err, friends) {
+        // console.log(friends);
+        friends.forEach(function (friend) {
+          client.rpush("user:" + friend + ":updateList", post.postId, function (err, reply) {
+            if (err) {
+              console.log("Error " + err);
+            }
+          })
+        });
+      })
+      .exec(function (err, replies) {
+        console.log("MULTI got " + replies.length + " replies");
+        replies.forEach(function (reply, index) {
+          console.log("Reply " + index + ": " + reply.toString());
+        });
       });
-    });
   })
 
-  res.send("posted");
+  res.send("Status update posted");
+});
+// Add a comment on a specific post of a specific user
+router.post('/user/:uid/addComment', (req, res) => {
+
+  var uid = req.params.uid;
+  var comment = {
+    commentId: req.query.postId + ":comment:",
+    message: req.query.message
+  };
+
+  // Check Redis is running without error
+  client.on("error", function (err) {
+    console.log("Error " + err);
+  });
+
+  // Every post has a comment list, every comment of the list is associated to a user
+  client.keys(comment.commentId + "*", function (err, replies) {
+    if (err) {
+      console.log("Error " + err);
+    }
+
+    comment.commentId += replies.length
+
+    client.multi()
+      // Create the comment using HSET with a specific message, id of original user and datetime
+      .hmset(comment.commentId, ["message", comment.message, "user", uid, "datetime", Date.now()], function (err, reply) {
+        if (err) {
+          console.log("Error " + err);
+        }
+      })
+      // Add the comment to user's comment list
+      .rpush("user:" + uid + ":commentList", comment.commentId, function (err, reply) {
+        if (err) {
+          console.log("Error " + err);
+        }
+      })
+      .exec(function (err, replies) {
+        console.log("MULTI got " + replies.length + " replies");
+        replies.forEach(function (reply, index) {
+          console.log("Reply " + index + ": " + reply.toString());
+        });
+      });
+  })
+
+  res.send("Comment posted");
 });
 
 ///////////////////// PUT /////////////////////////////
-// Test create usere
+// Create a new user
 router.put('/createUser/:uid', (req, res) => {
   var uid = "user:" + req.params.uid;
   var name = req.query.name;
